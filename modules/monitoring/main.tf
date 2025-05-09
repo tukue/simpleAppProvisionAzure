2ff0eed75b85dd0d478514c50624d5ca3dffd70c
Tukue Gebregergis <tukue.geb@gmail.com>
2025-05-09 21:59:27 +0200

    upgrading terraform  version

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
+
 
 # Configure Azure Provider
 provider "azurerm" {
