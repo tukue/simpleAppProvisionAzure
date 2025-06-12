6ca2378716768afcacc08f174db0706e50594146
Tukue Gebregergis <tukue.geb@gmail.com>
2025-06-12 11:25:13 +0200

    fixing terraform versions

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.70"
+      version = "~> 3.70"  
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
--- modules/networking/versions.tf
+++ modules/networking/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.70"
+      version = "~> 3.70"  # Match the root module version
     }
   }
   required_version = ">= 1.0.0"
